eb6bce08a368cb43759d8a96e02b9bf4f9c948bb
David Jarrett <djarret1@my.westga.edu>
2018-02-13 22:04:11 -0500

    Added documentation to CodeSnippetDataStore interface.

--- src/io/CodeSnippetDataStore.java
+++ src/io/CodeSnippetDataStore.java
@@ -3,11 +3,33 @@ package io;
 import javafx.collections.ObservableList;
 import model.CodeSnippet;
 
+/**
+ * Provides a consistent interface for reading and writing CodeSnippets from a data-source.
+ * @author 	David Jarrett
+ * @version	2/13/2018
+ */
 public interface CodeSnippetDataStore {
 
+	/**
+	 * Provided explicitly in order to facilitate lazy loading.
+	 */
 	void loadCodeSnippets();
+	
+	/**
+	 * Provides the CodeSnippet data in the data-store as an ObservableList<CodeSnippet>
+	 * @return The list of CodeSnippets.
+	 */
 	ObservableList<CodeSnippet> getCodeSnippetList();
+	
+	/**
+	 * Should write all code snippets to disk.
+	 */
 	void saveCodeSnippets();
+	
+	/**
+	 * Stores a provided CodeSnippet in the data-store.
+	 * @param snippet
+	 */
 	void writeCodeSnippet(CodeSnippet snippet);
 	
 }
