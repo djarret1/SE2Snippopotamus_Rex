f34b7c8df2bd150b59cf43dc61471044abb9d6bc
David Jarrett <djarret1@my.westga.edu>
2018-02-14 17:42:45 -0500

    Added search methods to Code object.

--- src/model/Code.java
+++ src/model/Code.java
@@ -12,6 +12,8 @@ import javafx.beans.property.StringProperty;
  */
 public class Code {
 
+	private static final char SPACE = ' ';
+	
 	private StringProperty codeText;
 	
 	/**
@@ -41,6 +43,56 @@ public class Code {
 		this.codeText.set(Objects.requireNonNull(codeText, "Codetext was null"));
 	}
 	
-	//search methods
+	/**
+	 * Returns true if this code object contains the supplied queryText in any
+	 * position or form, including text buried within substrings.
+	 * @param queryText The text being searched for.
+	 * @return True if queryText is found anywhere in the code, and false otherwise.
+	 */
+	public boolean containsText(String queryText) {
+		String codeText = this.codeText.get();
+		return codeText.contains(queryText);
+	}
+	
+	/**
+	 * Returns true if this code object contains the supplied queryText, exactly as
+	 * provided. It will not locate text buried within substrings.
+	 * @param queryText The text being searched for.
+	 * @return True if an exact match is found, and false otherwise.
+	 */
+	public boolean containsTextExactMatch(String queryText) {
+		String codeText = this.codeText.get();
+		int index = -1;
+		do {
+			index = codeText.indexOf(queryText, index + 1);
+			if(index != -1 && this.isNotEmbeddedText(codeText, queryText, index)) {
+				return true;
+			}
+		} while (index != -1);
+		return false;
+	}
+
+	private boolean isNotEmbeddedText(String codeText, String queryText, int index) {
+		if (codeText.length() == queryText.length()) {
+			return true;
+		}
+		if (index == 0) {
+			return this.checkEnd(codeText, queryText, index);
+		} else {
+			return this.checkBeginningAndEnd(codeText, queryText, index);
+		}
+	}
+
+	private boolean checkEnd(String codeText, String queryText, int index) {
+		if (index + queryText.length() == codeText.length()) {
+			return true;
+		} else {
+			return codeText.charAt(index + queryText.length()) == SPACE;
+		}
+	}
+	
+	private boolean checkBeginningAndEnd(String codeText, String queryText, int index) {
+		return codeText.charAt(index - 1) == SPACE && this.checkEnd(codeText, queryText, index);
+	}
 	
 }
