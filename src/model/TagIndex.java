8bbfc949a472ce0331146ecc4d8e1af288974aec
Weems <inquisitorcorgi@gmail.com>
2018-02-14 22:29:29 -0500

    Initial commit of the TagIndex.
    Initial commit of the TagIndex. Contains methods to add and remove tags
    from the index; tag a snippet, and index it; and purge all refrences to
    a tag.

---
+++ src/model/TagIndex.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TagIndex {
+	private HashMap<String, ArrayList<CodeSnippet>> tags;
+
+	public TagIndex() {
+		this.tags = new HashMap<String, ArrayList<CodeSnippet>>();
+	}
+
+	public void addTag(String toAdd) {
+
+		if (this.tags == null) {
+			throw new IllegalStateException("TagIndex not initialized");
+		}
+		if (!this.tags.containsKey(toAdd)) {
+			this.tags.put(toAdd, new ArrayList<CodeSnippet>());
+		}
+	}
+
+	public void tagSnippet(String tag, CodeSnippet snippet) {
+
+		if (this.tags == null) {
+			throw new IllegalStateException("TagIndex not initialized");
+		}
+		if (!this.tags.containsKey(tag)) {
+			this.addTag(tag);
+			this.tagSnippet(tag, snippet);
+		} else {
+			this.tags.get(tag).add(snippet);
+			snippet.addTag(tag);
+		}
+
+	}
+
+	public void purgeTag(String toPurge) {
+
+		if (this.tags == null) {
+			throw new IllegalStateException("TagStore not initialized");
+		}
+		if (this.tags.containsKey(toPurge)) {
+			for(CodeSnippet currSnippet: this.tags.get(toPurge) ){
+				currSnippet.removeTag(toPurge);
+			}
+			this.tags.remove(toPurge);
+		}
+	}
+
+	public void removeTag(String toRemove) {
+
+		if (this.tags == null) {
+			throw new IllegalStateException("TagStore not initialized");
+		}
+		if (this.tags.containsKey(toRemove)) {
+			this.tags.remove(toRemove);
+		}
+	}
+}
